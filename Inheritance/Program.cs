911d49eebe99dca2246bf0d8f2b9322e215fc643
Yakup İlyas Çağan <yakupilyascagan@gmail.com>
2021-03-22 22:26:06 +0300

    22/03/2021

---
+++ Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirsName="Engin"},
+                new Student{FirsName="Yakup" },
+                new Person{FirsName="Halo"}
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirsName);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Person2
+    {
+                  
+    }
+
+    class Customer : Person //interfaces ler 2 kere yazılabilir ancak inheritancesler yazılamaz
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Departmant { get; set; }
+    }
+}
